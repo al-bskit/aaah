eb6f3ee1679a96d9480fb4078523eacaaba990e5
HusseinDafer <hussein.mansour132.2000.5@gmail.com>
2020-09-08 15:38:04 +0300

    df

--- arajs_1.js
+++ arajs_1.js
@@ -1,4 +1,33 @@
-var marck = 60
-var pass
-pass = marck > 50? true:false
-console.log(pass)
+function sum() 
+    {
+        var n1 = parseInt( document.getElementById('txt_1').value);
+        var n2 = parseInt( document.getElementById('txt_2').value);
+        document.getElementById("txt_result").innerHTML = n1+n2;
+    }
+
+    var ar = ["hussein" , "yosif" , "ahmeed" ];
+
+    function gg(x,y)
+    {
+        return(x*y)
+    }
+    alert(ar[2])
+
+    var ara = gg(3,6)
+    document.write(ara);
+
+
+    function gay ()
+    {
+        alert("ha ha ha fucking gay");
+    }
+
+
+    function loop()
+    {
+
+        for(i=0;i<100;1++)
+        document.write(i);
+
+    }
+
